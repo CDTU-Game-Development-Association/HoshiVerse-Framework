473bef793812cd734a5a1b916be520c0cae543f4
Yumihoshi <xiaoyuesun915@gmail.com>
2025-04-09 13:28:16 +0800

    [docs] 修改消息中心文档，增加取消所有订阅方法
    Signed-off-by: Yumihoshi <xiaoyuesun915@gmail.com>

--- Assets/Scripts/HoshiVerseFramework/Base/EventCenterManager.cs
+++ Assets/Scripts/HoshiVerseFramework/Base/EventCenterManager.cs
@@ -15,7 +15,11 @@ namespace HoshiVerseFramework.Base
     {
         private readonly Dictionary<Type, Delegate> _eventHandlers = new();
 
-        // 订阅事件
+        /// <summary>
+        /// 订阅事件
+        /// </summary>
+        /// <param name="handler"></param>
+        /// <typeparam name="T"></typeparam>
         public void AddListener<T>(Action<T> handler)
         {
             Type type = typeof(T);
@@ -24,7 +28,12 @@ namespace HoshiVerseFramework.Base
                     Delegate.Combine(_eventHandlers[type], handler);
         }
 
-        // 触发事件
+
+        /// <summary>
+        /// 触发事件
+        /// </summary>
+        /// <param name="eventData"></param>
+        /// <typeparam name="T"></typeparam>
         public void TriggerEvent<T>(T eventData)
         {
             Type type = typeof(T);
@@ -32,7 +41,11 @@ namespace HoshiVerseFramework.Base
                 (handler as Action<T>)?.Invoke(eventData);
         }
 
-        // 取消订阅
+        /// <summary>
+        /// 取消订阅事件
+        /// </summary>
+        /// <param name="handler"></param>
+        /// <typeparam name="T"></typeparam>
         public void RemoveListener<T>(Action<T> handler)
         {
             Type type = typeof(T);
@@ -43,6 +56,25 @@ namespace HoshiVerseFramework.Base
                 _eventHandlers[type] = newDelegate;
             else
                 _eventHandlers.Remove(type);
+        }
+
+        /// <summary>
+        /// 取消某个类型的所有订阅
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public void RemoveCustomAllListeners<T>()
+        {
+            Type type = typeof(T);
+            if (_eventHandlers.ContainsKey(type))
+                _eventHandlers.Remove(type);
+        }
+
+        /// <summary>
+        /// 取消所有类型的所有订阅
+        /// </summary>
+        public void RemoveAllListeners()
+        {
+            _eventHandlers.Clear();
         }
     }
 }
